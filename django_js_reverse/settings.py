a18057d3d0303b58d335a41603c78505a2dcf7bd
Chow Loong Jin <hyperair@debian.org>
2014-02-14 11:17:54 +0800

    Use lazy objects for variables in .settings
    This avoids errors caused by stale setting values when testing with
    django.test.utils.override_settings.

--- django_js_reverse/settings.py
+++ django_js_reverse/settings.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.conf import settings
+from django.utils.functional import lazy
+from six import text_type as str
 
-JS_VAR_NAME = getattr(settings, 'JS_REVERSE_JS_VAR_NAME', 'Urls')
-
-JS_AVAILABLE_NAMESPACES = getattr(settings, 'JS_REVERSE_AVAILABLE_NAMESPACES', [])
+JS_VAR_NAME = lazy(
+    lambda: str(getattr(settings, 'JS_REVERSE_JS_VAR_NAME', 'Urls')),
+    str)()
+JS_AVAILABLE_NAMESPACES = lazy(
+    lambda: getattr(settings, 'JS_REVERSE_AVAILABLE_NAMESPACES', []),
+    list, tuple)()
