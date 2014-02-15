3ec9ad01b888e7c5e36c363ceede803fe8c82a56
Chow Loong Jin <hyperair@debian.org>
2014-02-15 23:52:08 +0800

    Recursively handle namespaced URLs

--- django_js_reverse/views.py
+++ django_js_reverse/views.py
@@ -1,11 +1,13 @@
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import re
-from itertools import chain
+from itertools import chain, count
 from django.core.exceptions import ImproperlyConfigured
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core import urlresolvers
+from six.moves import zip
+import six
 from .settings import JS_VAR_NAME, JS_AVAILABLE_NAMESPACES
 
 def urls_js(request):
@@ -20,21 +22,28 @@ def urls_js(request):
         if (isinstance(url_name, str) or isinstance(url_name, unicode))
     ]
 
-    default_urlresolver = urlresolvers.get_resolver(None)
-    # Prepare data for namespeced urls
-    named_urlresolves = [
-        (n_urlresolver, namespace_path, namespace + ':')
-        for namespace, (namespace_path, n_urlresolver) in default_urlresolver.namespace_dict.items()
-        if namespace in JS_AVAILABLE_NAMESPACES
-    ]
 
-    url_lists = [prepare_url_list(*args) for args in named_urlresolves]
-    # Add urls without namespaces
-    url_lists.append((prepare_url_list(default_urlresolver)))
+    def serialize_urlresolver(namespace, pathprefix, urlresolver):
+        nsprefix = '' if namespace is None else namespace + ':'
+        if namespace in JS_AVAILABLE_NAMESPACES or not namespace:
+            for url_name, url_pattern in urlresolver.reverse_dict.items():
+                if isinstance(url_name, basestring):
+                    yield (nsprefix + url_name, pathprefix, url_pattern[0][0])
+
+        for child_ns, (child_ns_path, child_urlresolver) in \
+            urlresolver.namespace_dict.items():
+
+            for args in serialize_urlresolver(nsprefix + child_ns,
+                                              pathprefix + child_ns_path,
+                                              child_urlresolver):
+                yield args
+
+    default_urlresolver = urlresolvers.get_resolver(None)
+    url_lists = serialize_urlresolver(None, '', default_urlresolver)
 
     return render_to_response('django_js_reverse/urls_js.tpl',
                               {
-                                  'urls': chain(*url_lists),
+                                  'urls': url_lists,
                                   'url_prefix': urlresolvers.get_script_prefix(),
                                   'js_var_name': JS_VAR_NAME
                               },
